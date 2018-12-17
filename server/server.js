3d6f5c8a4bfcbf4e1acd38963dcb71759201da8a
Daniel Díaz <dalejodc@gmail.com>
2018-12-16 23:35:01 -0600

    HTTP requests examples

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', (req, res)=> {
-  res.json('Hello World')
+
+app.get('/user', (req, res) => {
+    res.json('GET user')
 })
- 
-app.listen(3000, ()=>{
+
+app.post('/user', (req, res) => {
+    res.json('POST user')
+})
+
+app.put('/user/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    res.json({id})
+})
+
+app.delete('/user', (req, res) => {
+    res.json('DELETE user')
+})
+
+app.listen(3000, () => {
     console.log("Runnign in the port 3000");
 })
