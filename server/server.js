ace8723356be5f59dc3dabc8f817703ff11aa228
Daniel Díaz <dalejodc@gmail.com>
2018-12-17 23:25:50 -0600

    Connection created

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const express = require('express')
 const app = express()
 const bodyparser = require('body-parser')
+const mongoose = require('mongoose');
 
 app.get('/user', (req, res) => {
     res.json('GET user')
@@ -22,6 +23,12 @@ app.put('/user/:id', (req, res) => {
 app.delete('/user', (req, res) => {
     res.json('DELETE user')
 })
+
+mongoose.connect('mongodb://localhost:27017/products', (err, res)=>{
+    if(err) throw err;
+
+    console.log('DB started.');
+});
 
 app.listen(PORT, () => {
     console.log(`Runnign in the port ${PORT}`);
