f562f9e5148336a9de65acaa4793c283675457cc
Daniel Díaz <dalejodc@gmail.com>
2018-12-18 18:49:07 -0600

    User model

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    name:{
+        type: String, 
+        required: [true, 'The user name is required.']
+    },
+    email: {
+        type: String, 
+        required: [true, 'The email is required']
+    },
+    password: {
+        type: String, 
+        required: [true, 'The password is required']
+    },
+    image: {
+        type: String, 
+        required: false
+    },
+    rol:{
+        type: String,
+        required: true,
+        default: 'USER_ROLE'
+    },
+    google:{
+        type: boolean
+    },
+    state:{
+        type: boolean,
+        default: true
+    }
+});
