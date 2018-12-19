3aeb122256108e211c082bd46c14d7718ea63bd4
Daniel Díaz <dalejodc@gmail.com>
2018-12-18 23:41:42 -0600

    Data types changed

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 
+const User = require('../models/user')
+
 app.get('/user', (req, res) => {
     res.json('GET user')
 })
 
 app.post('/user', (req, res) => {
+    
+    let body = req.body;
+
+    let user = new User({
+        name:  body.name,
+        email: body.email,
+        password: body.password,
+        role: body.role,
+        status: true
+    })
+
     res.json('POST user')
 })
 
