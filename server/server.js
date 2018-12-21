c49ee44888364a70ff156609d5fc9104e11ed23f
Daniel Díaz <dalejodc@gmail.com>
2018-12-20 20:29:58 -0600

    Mongoose Deprecation warning fixed

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 //Global config of routes
 app.use(require('./routes/user'));
 
-mongoose.connect('mongodb://localhost:27017/products', (err, res)=>{
+mongoose.connect('mongodb://localhost:27017/products', { useNewUrlParser: true }, (err, res)=>{
     if(err) throw err;
 
     console.log('DB started.');
