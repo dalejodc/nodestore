3188cbeef2d704988564499f654ea6c71b5738f1
Daniel Díaz <dalejodc@gmail.com>
2018-12-21 22:29:52 -0600

    Getting the collection count

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,51 +6,55 @@ const _ = require('underscore');
 const User = require('../models/user')
 
 app.get('/users', (req, res) => {
-    
+
     let from = req.query.from || 0;
     from = Number(from);
-    
+
     let to = req.query.to || 5;
     to = Number(to);
-    
+
     User.find({})
         .skip(from)
         .limit(to)
-        .exec((err, users)=>{
-        if(err){
-            return res.status(400).json({
-                ok: false,
-                err: err
-            });
-        }
-        
-        res.json({
-            ok: true,
-            users: users
+        .exec((err, users) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err: err
+                });
+            }
+
+            User.count({}, (err, count) => {
+                res.json({
+                    ok: true,
+                    count: count,
+                    users: users
+                });
+            })
         });
-    });
 })
 
-app.post('/user', (req, res)=> {
-    
+app.post('/user', (req, res) => {
+
     let body = req.body;
 
     let user = new User({
-        name:  body.name,
+        name: body.name,
         email: body.email,
-        password: bcrypt.hashSync(body.password,10),
+        password: bcrypt.hashSync(body.password, 10),
         role: body.role
     })
-    
-    user.save( (err, userDB)=>{
-        
-        if(err){
+
+    user.save((err, userDB) => {
+
+        if (err) {
             return res.status(400).json({
                 ok: false,
                 err: err
             });
         }
-        
+
         res.json({
             ok: true,
             user: userDB
@@ -61,11 +65,11 @@ app.post('/user', (req, res)=> {
 app.put('/user/:id', (req, res) => {
 
     let id = req.params.id;
-    let body =_.pick(req.body, ['name','email', 'image', 'role', 'status']); //To select just the properties to update
+    let body = _.pick(req.body, ['name', 'email', 'image', 'role', 'status']); //To select just the properties to update
 
-    User.findByIdAndUpdate(id, body, {new: true, runValidators: true}, (err, user)=>{
+    User.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, user) => {
 
-        if(err){
+        if (err) {
             return res.status(400).json({
                 ok: false,
                 err: err
