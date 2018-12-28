0cd19a3ec97c31aac7dad71cd8a05883c9b7775e
Daniel Díaz <dalejodc@gmail.com>
2018-12-27 21:56:36 -0600

    Catching errors

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,11 +3,39 @@ const app = express()
 const bcrypt = require('bcrypt');
 const User = require('../models/user')
 
-app.post('/login', (req, res)=>{
+app.post('/login', (req, res) => {
 
-    res.json({
-        ok: true
-    })
+    let body = req.body;
+
+    User.findOne({ email: body.email }, (err, userDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err: err
+            });
+        }
+
+        if (!userDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'User incorrect'
+                }
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, userDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Incorrect Password'
+                }
+            });
+        }
+
+        
+    });
 });
 
 
