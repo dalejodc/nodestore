49a68e9ad2b66e7ea85d9fa2569b9052c75a1653
Daniel Díaz <dalejodc@gmail.com>
2018-12-28 20:16:13 -0600

    File renamed

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 //Global config of routes
-app.use(require('./routes/index'));
+app.use(require('./routes/routes'));
 
 mongoose.connect(process.env.DBURL, { useNewUrlParser: true }, (err, res)=>{
     if(err) throw err;
