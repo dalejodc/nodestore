95f1673170670b8b95c040e8987c7779bd2e7446
Daniel Díaz <dalejodc@gmail.com>
2019-01-14 22:49:26 -0600

    POST Category

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,5 +1,38 @@
 const express = require('express')
+const checkToken = require('../middlewares/authentication').checkToken
 const app = express()
 
-const checkToken = require('../middlewares/authentication').checkToken
 const Category = require('../models/category')
+
+app.post('/category', checkToken, (req, res)=>{
+    let body = req.body;
+    
+    let category = new Category({
+        description: body.description,
+        user: req.user._id
+    })
+
+    category.save((err, categoryDB)=>{
+        
+        if(err){
+            return res.status(500).json({
+                ok:false,
+                err: err
+            })
+        }
+
+        if(!categoryDB){
+            return res.status(400).json({
+                ok:false,
+                err: err
+            })
+        }
+
+        res.json({
+            ok:true,
+            category: categoryDB
+        })
+    })
+});
+
+module.exports = app;
