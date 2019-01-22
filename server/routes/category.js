faf4d19ed7a713835d304a24eb844cb7769005c4
Daniel Díaz <dalejodc@gmail.com>
2019-01-21 18:53:12 -0600

    PUT Category method

--- server/routes/category.js
+++ server/routes/category.js
@@ -35,4 +35,39 @@ app.post('/category', checkToken, (req, res)=>{
     })
 });
 
+//Method to update a category
+app.put('/category/:id', checkToken, (req, res) => {
+
+    let id = req.params.id;
+    let body = req.body
+
+    let category = {
+        description: body.description
+    };
+
+    Category.findByIdAndUpdate(id, category, { new: true, runValidators: true }, (err, categoryDB) => {
+
+        if(err){
+            return res.status(500).json({
+                ok:false,
+                err: err
+            })
+        }
+
+        if(!categoryDB){
+            return res.status(400).json({
+                ok:false,
+                err: err
+            })
+        }
+
+        res.json({
+            ok:true,
+            category: categoryDB
+        })
+
+    });
+
+})
+
 module.exports = app;
