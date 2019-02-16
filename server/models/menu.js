86568c2a1569ea09c43a5dd5714bd97a273b6d05
Daniel Díaz <dalejodc@gmail.com>
2019-02-15 22:52:46 -0600

    Menu schema

---
+++ server/models/menu.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var menuSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    url:{
+        type: String,
+        required: true
+    },
+    icon:{
+        type: String,
+        required: false
+    }
+})
